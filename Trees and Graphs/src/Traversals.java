0338ff52967fa96fb43d271196f28444a80ab738
Snehal Sutar <svs.sutar.1@gmail.com>
2016-07-21 10:05:05 -0500

    Initializing the tree.

--- Trees and Graphs/src/Traversals.java
+++ Trees and Graphs/src/Traversals.java
@@ -2,8 +2,25 @@
 public class Traversals {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Node head = new Node(0);
+		initializeTree(head);
 
+	}
+
+	private static void initializeTree(Node head) {
+		int[] numArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+		for (int i = 1; i < numArray.length - 1; i++) {
+			Node nextNodeLeft = new Node(numArray[i++]);
+			Node nextNodeRight = new Node(numArray[i++]);
+			head.left = nextNodeLeft;
+			head.right = nextNodeRight;
+
+			if (head.left != null && head.right != null)
+				head = head.left;
+			else if (head.left != null && head.right == null)
+				head = head.right;
+		}
 	}
 
 }
